f049438b7732884eccf17905dbcb8d95b9526c8d
Purici Marius <puricimarius@gmail.com>
2019-12-24 18:22:18 +0200

    ytsTelegramBot

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using YtsClient;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace YtsTelegramBot
+{
+    class Program
+    {
+        static readonly string API_KEY = "889160236:AAElNTg18z6EXqGlvv3cMDJ3wJBztZtHd00";
+        static readonly TelegramBotClient Bot = new TelegramBotClient(API_KEY);
+
+        static void Main(string[] args)
+        {
+            string username = Bot.GetMeAsync().Result.Username;
+            System.Console.Title = username;
+
+            Bot.OnMessage += OnMessageReceived;
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            Bot.StartReceiving();
+            System.Console.WriteLine($"The Bot {username} has started receiving messages");
+            System.Console.WriteLine("Press Q to quit...");
+            while (true)
+            {
+                System.ConsoleKeyInfo cki = System.Console.ReadKey(true);
+                if(cki.Key == System.ConsoleKey.Q) break;
+            }
+            Bot.StopReceiving();
+            System.Console.WriteLine($"The Bot {username} has stopped receiving messages");
+        }
+
+        static async void OnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message == null || message.Type != MessageType.Text) return;
+
+            await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+            switch (message.Text)
+            {
+                case "/start":
+                case "/help":
+                    await Bot.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: "Welcome to ytsTelegramBot by Markus_17\n" +
+                        "Send me any movie title and I will try to find it for you\n" +
+                        "Send /help to see this message again"
+                    );
+                    break;
+                default:
+                    var res = await Request.Make(query_term: message.Text, page: 1, limit: 50);
+
+                    if(res.data.movie_count == 0)
+                    {
+                        await Bot.SendTextMessageAsync(
+                            chatId: message.Chat.Id,
+                            text: "No such movie was found..."
+                        );
+                        return;
+                    }
+
+                    string output = "";
+                    var buttons = new List<InlineKeyboardButton>();
+                    for (int i = 0; i < res.data.movies.Count; i++)
+                    {
+                        output += $"{i+1}) {res.data.movies[i].title_long}\n";
+                        buttons.Add(new InlineKeyboardButton{Text = $"{i+1}", CallbackData = $"{res.data.movies[0].id}"});
+                    }
+
+                    var inlineKeyboard = new InlineKeyboardMarkup(buttons.ToArray());
+
+                    await Bot.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: output,
+                        replyMarkup: inlineKeyboard
+                    );
+                    break;
+            }
+        } 
+
+        static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            var callbackQuery = callbackQueryEventArgs.CallbackQuery;
+            await Bot.AnswerCallbackQueryAsync(
+                callbackQueryId: callbackQuery.Id,
+                text: $"ID {callbackQuery.Data}"
+            );
+            var resMovie = await Request.FindMovieById(callbackQuery.Data);
+
+            var buttons = new List<InlineKeyboardButton>();
+            for (int i = 0; i < resMovie.data.movie.torrents.Count; i++)
+            {
+                buttons.Add(new InlineKeyboardButton{Text = resMovie.data.movie.torrents[i].quality,  Url = resMovie.data.movie.torrents[i].url});
+            }
+
+            await Bot.SendPhotoAsync(
+                chatId: callbackQuery.Message.Chat.Id,
+                photo: resMovie.data.movie.large_cover_image,
+                caption: resMovie.data.movie.description_full,
+                replyMarkup: new InlineKeyboardMarkup(buttons.ToArray())
+            );
+        }
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            System.Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message
+            );
+        }
+    }
+}
---
+++ ytsClient.cs
@@ -0,0 +1,77 @@
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace YtsClient
+{
+    class Request
+    {
+        static readonly HttpClient hc = new HttpClient();
+        
+        // query_term - Used for movie search, matching on: Movie Title/IMDb Code, Actor Name/IMDb Code, Director Name/IMDb Code
+        // limit - The limit of results per page that has been set
+        // page - Used to see the next page of movies, eg limit=15 and page=2 will show you movies 15-30
+        // sort_by - String (title, year, rating, peers, seeds, download_count, like_count, date_added) Sorts the results by choosen value
+        public static async Task<JsonResponse> Make(string query_term, int page, int limit, string sort_by = "title")
+        {
+            HttpResponseMessage response = await hc.GetAsync($"https://yts.lt/api/v2/list_movies.json?query_term={query_term}&page={page}&limit={limit}&sort_by={sort_by}");
+            // Throws an exception if the System.Net.Http.HttpResponseMessage.IsSuccessStatusCode property for the HTTP response is false.
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Converts the json string to a JsonResponse object
+            return JsonConvert.DeserializeObject<JsonResponse>(responseBody);
+        }
+
+        public static async Task<JsonResponseMovie> FindMovieById(string id)
+        {
+            HttpResponseMessage response = await hc.GetAsync($"https://yts.lt/api/v2/movie_details.json?movie_id={id}");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<JsonResponseMovie>(responseBody);
+        }
+    }
+
+    class JsonResponseMovie
+    {
+        public string status { get; set; }
+        public string status_message { get; set; }
+        public MovieData data { get; set; }
+    }
+
+    class MovieData
+    {
+        public Movie movie { get; set; }
+    }
+
+    class JsonResponse
+    {
+        public string status { get; set; }
+        public string status_message { get; set; }
+        public MovieList data { get; set; }
+    }
+
+    class MovieList
+    {
+        public int movie_count { get; set; }
+        public int limit { get; set; }
+        public int page_number { get; set; } 
+        public List<Movie> movies { get; set; }
+    }
+
+    class Movie
+    {
+        public int id { get; set; }
+        public string title_long { get; set; }
+        public string description_full { get; set; }
+        public string large_cover_image { get; set; }
+        public List<Torrent> torrents { get; set; }
+    }
+
+    class Torrent {
+        public string url { get; set; }
+        public string quality { get; set; } 
+    }
+}
